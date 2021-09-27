5d982616206a62a50aee4e01940670de1a2ea55f
tanya0311 <tanya03.11@mail.ru>
2021-09-27 21:44:37 +0300

    contactForm

--- src/contacts/Contact.tsx
+++ src/contacts/Contact.tsx
@@ -3,38 +3,69 @@ import s from "./Contacts.module.scss"
 import styleContainer from "./../common/styles/Container.module.css"
 import Title from "../components/Title/Title"
 import { Fade } from "react-awesome-reveal"
-
+import { useFormik } from "formik"
+import axios from 'axios'
 
 function Contacts() {
+
+
+	type FormikValues = {
+		email: string
+		name: string
+		message: string
+  }
+ 
+  const formik = useFormik<FormikValues>({
+		initialValues: {
+			 email: '',
+			 name: '',
+			 message: ''
+		},
+		
+		onSubmit: values => {
+			// alert(JSON.stringify(values) )
+			let {name, email, message} = values
+
+			axios.post('http://localhost:3010/sendMessage', {name, email, message})
+			// axios.post('https://glacial-falls-34953.herokuapp.com/sendMessage', {name, email, message})
+				 .then(() => {
+					//  alert('hello')
+					  formik.resetForm()
+				 })
+				
+		}
+  })
+
+
 	return (
 		<div id='contacts' className={s.contactsBlock}>
 			<div className={`${styleContainer.container} ${s.contactsContainer}`}>
 				<Title title={"Contacts"} />
-				<Fade direction='up' >
-				<form className={s.contactForm}>
-					<input
-						className={s.contactFormInput}
-						type='text'
-						placeholder={"Name"}
-					/>
-					<input
-						className={s.contactFormInput}
-						type='text'
-						placeholder={"Email"}
-					/>
-					<textarea
-						className={s.contactFormInput}
-						name=''
-						placeholder={"Message"}
-					></textarea>
-				</form>
-				
-				</Fade  >
-				
-				<button className={s.submitBtn} type={"submit"}>
-					Send
-				</button>
-				
+				<Fade direction='up'>
+					<form className={s.contactForm} onSubmit={formik.handleSubmit}>
+						<input
+							className={s.contactFormInput}
+							type='text'
+							placeholder={"Name"}
+							{...formik.getFieldProps('name')}
+						/>
+						<input
+							className={s.contactFormInput}
+							type='text'
+							placeholder={"Email"}
+							{...formik.getFieldProps('email')}
+						/>
+						<textarea
+							className={s.contactFormInput}
+							placeholder={"Message"}
+							{...formik.getFieldProps('message')}
+						></textarea>
+
+						<button className={s.submitBtn} type={"submit"}>
+							Send
+						</button>
+					</form>
+				</Fade>
 			</div>
 		</div>
 	)
